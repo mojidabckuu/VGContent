6cd63c0823de87e1efff0ee7680488be64800417
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-07-07 09:10:37 +0400

    Add properties to view controller.

--- Pod/Classes/ViewControllers/VGContentViewController/VGContentViewController.h
+++ Pod/Classes/ViewControllers/VGContentViewController/VGContentViewController.h
@@ -12,5 +12,11 @@
 
 @interface VGContentViewController : UIViewController <VGContentControllerProtocol>
 
+@property (nonatomic, weak) IBOutlet UIView *contentView;
+
+@property (nonatomic, strong) VGURLContent *content;
+
+@property (nonatomic, strong) IBInspectable NSString *contentName;
+@property (nonatomic, strong) IBInspectable NSString *cellIdentifier;
 
 @end
