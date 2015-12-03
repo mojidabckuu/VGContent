d9a73e2533c6a81fc2d086964c5b4633bf5ca2fd
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2015-12-03 12:44:07 +0400

    Add cells category propertyies.

--- Pod/Classes/Models/VGCollectionViewContent/UICollectionViewCell+Setup.m
+++ Pod/Classes/Models/VGCollectionViewContent/UICollectionViewCell+Setup.m
@@ -8,9 +8,25 @@
 
 #import "UICollectionViewCell+Setup.h"
 
+#import <objc/runtime.h>
+
 @implementation UICollectionViewCell (Setup)
 
+#pragma mark - Setup
+
 - (void)setupWithItem:(id)item {
+}
+
+#pragma mark - Accessors
+
+- (VGContent *)content {
+    return objc_getAssociatedObject(self, @selector(content));
+}
+
+#pragma mark - Modifiers
+
+- (void)setContent:(VGContent *)content {
+    objc_setAssociatedObject(self, @selector(content), content, OBJC_ASSOCIATION_ASSIGN);
 }
 
 @end
