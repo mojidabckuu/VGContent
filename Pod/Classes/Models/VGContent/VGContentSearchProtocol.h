6d8a8bfdcce7afd519c05a86028cead202b8bab6
Vlad Gorbenko <mojidabckuu.22.06.92@gmail.com>
2016-06-26 16:25:14 +0400

    update search content nullability

--- Pod/Classes/Models/VGContent/VGContentSearchProtocol.h
+++ Pod/Classes/Models/VGContent/VGContentSearchProtocol.h
@@ -13,25 +13,25 @@
 /**
  `NSArray` of original items.
  */
-@property (nonatomic, strong) NSMutableArray *originalItems;
+@property (nonatomic, strong, nullable) NSMutableArray *originalItems;
 
 /**
  `NSArray` of fildeted items.
  */
-@property (nonatomic, strong) NSArray *filteredItems;
+@property (nonatomic, strong, nullable) NSArray *filteredItems;
 
 /**
  Method to perform search on items.
  @result Result array will be putted to filtered items.
  */
-- (void)searchWithString:(NSString *)string;
+- (void)searchWithString:(nonnull NSString *)string;
 
 /**
  Method to perform search on items using keyPath on item.
  @param string Search text.
  @param keyPath Path to property of item.
  */
-- (void)searchWithString:(NSString *)string keyPath:(NSString *)keyPath;
+- (void)searchWithString:(nonnull NSString *)string keyPath:(nonnull NSString *)keyPath;
 
 /**
  Method to cancel search and remove cached items.
